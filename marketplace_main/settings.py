90cfd0f13eb56647fa7dd6b1b35158f78b12b6a2
Jade-hm <evelynjade09@gmail.com>
2026-05-22 16:07:09 -0700

    arreglando problemas

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bfdxwa4j=p=qgqlwu#xh4pxd8h)(=c=$3wl5+$f+cbqr(c(g92'
+SECRET_KEY = 'django-insecure-t%4)uz_+as#yi)_(lusxt*d!7944e$h03z-sz2-7l=z5kg(ynk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'store.User'
 
 ROOT_URLCONF = 'marketplace_main.urls'
 
@@ -115,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
